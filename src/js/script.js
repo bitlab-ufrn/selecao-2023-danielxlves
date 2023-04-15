fa4cef135b8ecb2c5967d0b28051de22258cd765
Daniel Alves <116274393+danielxlves@users.noreply.github.com>
2023-04-15 15:44:20 -0300

    terceiro commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,23 @@
 function verificarTexto() {
-  const texto = document.getElementById('texto').value //Captura texto do input
-  axios.post('http://localhost:5500/verificar-texto', { texto }) // Asa a api axios para enviar um post diretamente pelo localhost do server
-    .then(response => { //Caso nao tenha o erro esperado é por quê não há palavras ofensivas.
+const texto = document.getElementById('texto').value 
+
+  //consultando a api se há palavras ofensivas
+
+  axios.post('http://localhost:8080/verificar-texto', { texto }) 
+    .then(response => { 
       const resultado = document.getElementById('resultado'); 
-      if (response && response.data) { //Verificar se há retorno correto do servidor
+      if (response && response.data) { 
         resultado.innerHTML = `<p class="success">${response.data.message}</p>`;
       } else {
-        resultado.innerHTML = `<p class="error">Resposta inválida do servidor.</p>`;
+        resultado.innerHTML = `<p class="error">Resposta inválida do servidor.</p>`
       }
     })
-    .catch(error => { // Ocorrendo o erro esperado é por quê há palavras ofensivas
+    .catch(error => { 
       const resultado = document.getElementById('resultado');
       if (error && error.response && error.response.data && error.response.data.error) { 
-        resultado.innerHTML = `<p class="error">${error.response.data.error}</p>`; //Error esperado
+        resultado.innerHTML = `<p class="error">${error.response.data.error}</p>`
       } else {
-        resultado.innerHTML = `<p class="error">Erro na requisição.</p>`; //Error de requisicao
+        resultado.innerHTML = `<p class="error">Erro na requisição.</p>`
       }
     });
 }
